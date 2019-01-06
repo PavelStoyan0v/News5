cbbfe1b5608e0c647fc475b7923510b054dbdeba
PavelStoyanov <32341455+PavelStoyan0v@users.noreply.github.com>
2019-01-06 14:33:00 +0200

    started work on admin panel

--- src/Controller/ArticleController.php
+++ src/Controller/ArticleController.php
@@ -28,4 +28,46 @@ class ArticleController extends AbstractController
             'featured' => $featured
         ]);
     }
+
+    /**
+     * @Route("/admin/articles", name="adminArticles")
+     */
+    public function adminArticles()
+    {
+        $articleRepository = $this->getDoctrine()->getRepository(Article::class);
+        $articles = $articleRepository->findAll();
+
+        return $this->render('admin/articles.html.twig', [
+            'articles' => $articles
+        ]);
+    }
+
+    /**
+     * @Route("/admin/articles/edit/{id}")
+     */
+    public function editArticle($id)
+    {
+        $categoryRepository = $this->getDoctrine()->getRepository(Category::class);
+        $articleRepository = $this->getDoctrine()->getRepository(Article::class);
+        $article = $articleRepository->find($id);
+        $categories = $categoryRepository->findAll();
+
+        return $this->render('admin/article-edit.html.twig', [
+            'categories' => $categories,
+            'article' => $article
+        ]);
+    }
+
+    /**
+     * @Route("/admin/articles/new")
+     */
+    public function newArticle()
+    {
+        $categoryRepository = $this->getDoctrine()->getRepository(Category::class);
+        $categories = $categoryRepository->findAll();
+
+        return $this->render('admin/article-new.html.twig',[
+            'categories' => $categories
+        ]);
+    }
 }
